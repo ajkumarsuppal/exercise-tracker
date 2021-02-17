dd868191067c0b9931f6ba95a158f3088fa55d71
Ajay Suppal <akrsuppal@gmail.com>
2021-02-17 22:37:15 +0530

    start server success

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+//env. var in dotenv files
 require('dotenv').config();
 
+//create express server
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middleware
 app.use(cors());
+
+//parse json 
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log('Server is running on port: ')
+    console.log(`Server is running on port: ${port}`);
 })
