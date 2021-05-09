1761f9b851da6cdb2d5eab9596fe2e0db45f0231
Ajay Suppal <akrsuppal@gmail.com>
2021-05-10 02:13:57 +0530

    add components for frontend incl. navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 // import logo from './logo.svg';
 // import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Route} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component"
+import EditExercise from "./components/edit-exercise.component"
+import CreateExercise from "./components/create-exercise.component"
+import CreateUser from "./components/create-user.component"
 
 function App() {
   return (
     <Router>
-    <div className="container">
-      Hello world!
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
     </div>
     </Router>
   );
